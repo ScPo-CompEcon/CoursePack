2c650b4cd2f1d159746d1d20fe15396dbc5add73
florian oswald <florian.oswald@gmail.com>
2017-02-03 13:19:00 +0100

    update

---
+++ src/CoursePack.jl
@@ -0,0 +1,23 @@
+module slides
+
+function find_easter()
+
+println("""
+Welcome to the ScPo-CompEcon Easter Egg hunt!
+
+If you found this, the you have not successfully checked out
+the Easter branch of the repository. Please go through the
+materials again to find out how to change the branch of
+of a repository, or ask for help!
+""")
+end
+
+function hello()
+    println("""
+        Welcome to the ScPo-CompEcon slides repository!
+        """)
+end
+
+export find_easter, hello
+
+end
--- src/JupyterSite.jl
+++
@@ -1,3 +0,0 @@
-module JupyterSite
-
-end
